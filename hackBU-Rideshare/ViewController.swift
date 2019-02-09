d90cc66778f34adf594bfb40c7b3d0366f78e303
Matthew Reid <merknicks11@gmail.com>
2019-02-09 14:15:44 -0500

    Initial Commit

---
+++ hackBU-Rideshare/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hackBU-Rideshare
+//
+//  Created by Matthew Reid on 2/9/19.
+//  Copyright © 2019 Matthew Reid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ hackBU-RideshareTests/hackBU_RideshareTests.swift
@@ -0,0 +1,36 @@
+//
+//  hackBU_RideshareTests.swift
+//  hackBU-RideshareTests
+//
+//  Created by Matthew Reid on 2/9/19.
+//  Copyright © 2019 Matthew Reid. All rights reserved.
+//
+
+import XCTest
+@testable import hackBU_Rideshare
+
+class hackBU_RideshareTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
